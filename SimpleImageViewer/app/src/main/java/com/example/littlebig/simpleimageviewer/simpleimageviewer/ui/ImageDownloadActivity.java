61ba75f0a3a184be33bbea378eb8bdc62d09001a
askazoobee <felipe.ascazubi@gmail.com>
2016-03-17 18:13:38 -0400

    clean up

--- SimpleImageViewer/app/src/main/java/com/example/littlebig/simpleimageviewer/simpleimageviewer/ui/ImageDownloadActivity.java
+++ SimpleImageViewer/app/src/main/java/com/example/littlebig/simpleimageviewer/simpleimageviewer/ui/ImageDownloadActivity.java
@@ -26,7 +26,6 @@ public class ImageDownloadActivity extends AppCompatActivity {
         final Button downloadButton = (Button) findViewById(R.id.download_button);
         final EditText urlEdit = (EditText) findViewById(R.id.url_edit);
 
-
         startDownload(downloadButton, urlEdit);
 
     }
@@ -37,26 +36,19 @@ public class ImageDownloadActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 if (!Utilities.isNetworkAvailable(getApplicationContext())) {
-                    int duration = Toast.LENGTH_SHORT;
-                    //Stop any previous toasts/tip from reviewer
-                    if (mToast != null) {
-                        mToast.cancel();
-                    }
                     //Make and display new toast
-                    mToast = Toast.makeText(getApplicationContext(), getString(R.string.error_connection), duration);
+                    mToast = Toast.makeText(getApplicationContext(), getString(R.string.error_connection), Toast.LENGTH_SHORT);
                     mToast.show();
                 } else {
                     final String url = e.getText().toString();
                     HttpUrl downloadUrl = HttpUrl.parse(url);
+
                     if(downloadUrl!=null) {
                         ImageDownloaderTask task = new ImageDownloaderTask(ImageDownloadActivity.this, getApplicationContext());
                         task.execute(downloadUrl);
-                    }
-                    else{
-                        Toast toast;
-                        String message = getString(R.string.error_download);
-                        toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
-                        toast.show();
+                    } else {
+                        mToast = Toast.makeText(getApplicationContext(), getString(R.string.error_download), Toast.LENGTH_SHORT);
+                        mToast.show();
                     }
                 }
             }
